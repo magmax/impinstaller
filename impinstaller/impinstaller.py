91afffaf6fe0e882a7c5e75c9d0b626a65001ca5
Miguel Ángel García <miguelangel.garcia@gmail.com>
2011-06-02 21:41:13 +0200

    first green

---
+++ impinstaller/impinstaller.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+# -*- coding:utf-8; tab-width:4; mode:python -*-
+
+print " FileContains(['Version', 'ImpInstaller', 'Miguel Ángel García'])"
---
+++ test/constants.py
@@ -0,0 +1,3 @@
+# -*- coding:utf-8; tab-width:4; mode:python -*-
+
+MAINEXE = '../impinstaller/impinstaller.py'
